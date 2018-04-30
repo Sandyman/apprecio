566d289db9e183170b9f945d68acbce769d2b8d7
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-30 17:04:30 +0800

    Allow sending tweet too in stead of only DM.

--- src/js/layouts/share.js
+++ src/js/layouts/share.js
@@ -1,15 +1,57 @@
 import React, { Component, Fragment } from "react";
 import urlencode from "urlencode";
 
-import { Twitter, X, Share2 } from 'react-feather';
+import { MessageSquare, Twitter, X, Share2 } from 'react-feather';
+
+const TWURL = 'https://twitter.com';
+const HASH_TAG = '#apprecio';
 
 class Share extends Component {
-  static shareLinks(word, pathname) {
-    const url = `https://www.apprecio.life${pathname}`;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasInput: false,
+      provider: '',
+      submitDisabled: true,
+    };
+
+    this.onProviderClick = this.onProviderClick.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.shareLinks = this.shareLinks.bind(this);
+  }
+
+  onInputChange() {
     const name = document.getElementById('name').value;
+    this.setState({
+      hasInput: name.length > 0,
+    })
+  }
+
+  onProviderClick() {
+    this.setState({
+      provider: document.querySelector('input[name="provider"]:checked').value,
+      submitDisabled: false,
+    })
+  };
+
+  shareLinks(word, pathname) {
+    const url = `https://www.apprecio.life${pathname}`;
+    let name = document.getElementById('name').value;
     if (name.length > 0) {
-      const baseUrl = 'https://twitter.com/messages/compose?text=';
-      const text = `Hi ${name}, I just wanted to let you know that I think you are ${word}. (${url})`;
+      const provider = this.state.provider;
+      let baseUrl;
+      let text;
+      if (provider === 'dm') {
+        baseUrl = `${TWURL}/messages/compose?text=`;
+        text = `Hi ${name}, I just wanted to let you know that I think you are ${word}. (${url})`;
+      } else {
+        if (name[0] !== '@') {
+          name = `@${name}`;
+        }
+        baseUrl = `${TWURL}/home?status=`;
+        text = `${name} I just wanted to let you know that I think you are ${word}. (${url}) ${HASH_TAG}`;
+      }
       const articleLink = `${baseUrl}${urlencode(text)}`;
       if (articleLink) {
         window.open(articleLink, '_blank');
@@ -18,6 +60,8 @@ class Share extends Component {
   };
 
   render() {
+    const { word, pathname } = this.props;
+    const { provider, submitDisabled, hasInput } = this.state;
     return (
       <Fragment>
         <div className="share icons">
@@ -25,14 +69,25 @@ class Share extends Component {
         </div>
         <div id="share" className="share-modal">
           <a href="#" className="close"><X /></a>
+          <div className="icons">
+            <label>
+                <input type="radio" name="provider" value="tweet" onClick={this.onProviderClick} /><Twitter />
+            </label>
+            <label>
+              <input type="radio" name="provider" value="dm" onClick={this.onProviderClick} /><MessageSquare />
+            </label>
+          </div>
           <div className="body">Let someone know that you think they are <strong>{this.props.word}</strong>.<br/>
             <span className="edit">(You can edit your message before sending.)</span>
           </div>
-
-          <input id="name" type="text" placeholder="What is their name?" />
-          <button type="button" onClick={() => Share.shareLinks(this.props.word, this.props.pathname)}>
+          <input id="name" type="text" placeholder="What is their name?" onChange={this.onInputChange} />
+          <button type="button" disabled={submitDisabled || !hasInput} onClick={() => this.shareLinks(word, pathname)}>
             <span className="twitter"><Twitter /></span>
-            Send them some love
+            {provider === 'dm'
+              ? 'Send Direct Message'
+              : provider === 'tweet'
+                ? 'Tweet'
+                : 'Send them some love'}
           </button>
         </div>
       </Fragment>
